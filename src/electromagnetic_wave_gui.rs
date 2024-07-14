05baf193dad75f2e118024886e96913c35ababab
Ivan Schuetz <ivanschuetz@gmail.com>
2024-07-14 18:36:50 +0200

    complete info labels

--- src/electromagnetic_wave_gui.rs
+++ src/electromagnetic_wave_gui.rs
@@ -11,9 +11,21 @@ fn add_info_labels(mut commands: Commands, font: &Handle<Font>) {
     commands.spawn(generate_info_label(&font, "move left: d", 20.0));
     commands.spawn(generate_info_label(&font, "zoom in: w", 40.0));
     commands.spawn(generate_info_label(&font, "zoom out: s", 60.0));
-    commands.spawn(generate_info_label(&font, "rotate around z: i", 80.0));
-    commands.spawn(generate_info_label(&font, "rotate around y: o", 100.0));
-    commands.spawn(generate_info_label(&font, "rotate around x: p", 120.0));
+    commands.spawn(generate_info_label(
+        &font,
+        "rotate around z: i / shift-i",
+        80.0,
+    ));
+    commands.spawn(generate_info_label(
+        &font,
+        "rotate around y: o / shift-o",
+        100.0,
+    ));
+    commands.spawn(generate_info_label(
+        &font,
+        "rotate around x: p / shift-p",
+        120.0,
+    ));
 }
 fn generate_info_label(font: &Handle<Font>, label: &str, top: f32) -> TextBundle {
     TextBundle {
